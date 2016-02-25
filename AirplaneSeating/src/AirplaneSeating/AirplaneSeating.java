063196cc8e9ac68179ba327ab8bbf43bf2ef1b8e
Misindexed Orange Hat <Konnoke@users.noreply.github.com>
2016-02-25 04:11:54 -0500

    Added files via upload

---
+++ AirplaneSeating/src/AirplaneSeating/AirplaneSeating.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AirplaneSeating;
+import java.util.Scanner;
+/**
+ *
+ * @author Konnoke
+ */
+public class AirplaneSeating {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Airplane plane = new Airplane();
+        Scanner scnr = new Scanner(System.in);
+        //String command = scnr.nextLine();
+        
+        plane.showSeating();
+        
+        
+        
+        
+        
+    }
+    
+}
